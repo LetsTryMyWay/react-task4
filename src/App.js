f255c19f957f9beaef2b79b698392f86f018eac6
LetsTryMyWay <mukeshverma20@gmail.com>
2023-10-14 20:36:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
 function App() {
+  const [columns, setColumns] = useState([]);
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/Data.json')
+      .then(res => res.json())
+      .then(data => {
+        setColumns(Object.keys(data.users[0]).filter(column => !['ein', 'ssn', 'userAgent'].includes(column)));
+        setRecords(data.users);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4" style={{ backgroundColor: "#343a40", color: "white", padding: "20px" }}>
+      <h1 style={{ color: "white" }}>User Data</h1>
+      <table className="table table-bordered table-dark">
+        <thead>
+          <tr>
+            {columns.map((column, i) => (
+              <th key={i}>{column}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {records.map((record, i) => (
+            <tr key={i}>
+              <td>{record.maidenName}</td>
+              <td>{record.age}</td>
+              <td>{record.gender}</td>
+              <td>{record.email}</td>
+              <td>{record.phone}</td>
+              <td>{record.username}</td>
+              <td>{record.password}</td>
+              <td>{record.birthDate}</td>
+              <td><img src={record.image} alt="user" style={{ maxWidth: '50px', maxHeight: '50px' }} /></td>
+              <td>{record.bloodGroup}</td>
+              <td>{record.height}</td>
+              <td>{record.weight}</td>
+              <td>{record.eyeColor}</td>
+              <td>{record.hair.color} - {record.hair.type}</td>
+              <td>{record.domain}</td>
+              <td>{record.ip}</td>
+              <td>{record.address.address}, {record.address.city}, {record.address.state}, {record.address.postalCode}</td>
+              <td>{record.macAddress}</td>
+              <td>{record.university}</td>
+              <td>{record.bank.cardNumber}</td>
+              <td>{record.company.name}, {record.company.department}, {record.company.title}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
